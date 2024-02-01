eb3b6dba2b530062d4bec4d3c8722f10147e86be
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-02-01 14:04:16 -0500

    creating keyboard functionality i suceeded in creating the board and the starting and the red cell

--- vite-project/app.js
+++ vite-project/app.js
@@ -4,26 +4,31 @@ import "./style.css";
 const gameBoard = () => {
   // Queryselectors
   const startButton = document.querySelector("#start-button");
-  const body = document.querySelector("#body");
   const h1Element = document.querySelector("#start-text");
   const startButtonContainer = document.querySelector(
     "#start-button-container"
   );
+  const easyBoardContainer = document.querySelector("#easy-board-container");
   // Array to store our cells
-  let cellArray = [];
-  const createGrid = () => {
+  const removeStart = () => {
     // Removing Our Elements To Start The Game
-    body.removeChild(h1Element)
-    body.removeChild(startButtonContainer);
+    h1Element.style.display = "none";
+    startButtonContainer.style.display = "none";
 
     //This allows us to keep our board hidden until our event listener is clicked
-    const easyBoardContainer = document.querySelector("#easy-board-container");
     easyBoardContainer.classList.replace("hidden", "block");
 
     // this also allows us to keep our information hidden until our event listener is clicked
     const boardInformation = document.querySelector("#board-information");
     boardInformation.classList.replace("hidden", "block");
+  };
+  //Array board to keep track of our cells
+  let cellArray = [];
+  let playerGreenCell;
+  let randomRedCell;
 
+  const createGrid = () => {
+    removeStart();
     for (let i = 0; i < 256; i++) {
       const cell = document.createElement("div");
       cell.className = "border border-white";
@@ -34,35 +39,34 @@ const gameBoard = () => {
     }
     // This would allow us to have our green starting point
     let randomGreenIndex = Math.floor(Math.random() * cellArray.length);
-    let randomGreenCell = cellArray[randomGreenIndex];
-    randomGreenCell.classList.add("bg-green-600");
+    playerGreenCell = cellArray[randomGreenIndex];
+    playerGreenCell.classList.add("bg-green-600");
 
     // This would allow us to have our red starting point
     let randomRedIndex = Math.floor(Math.random() * cellArray.length);
-    let randomRedCell = cellArray[randomRedIndex];
+    randomRedCell = cellArray[randomRedIndex];
     randomRedCell.classList.add("bg-red-600");
 
     // Incase if both random indexes land on the same one
-    if (randomGreenCell === randomRedCell) {
+    if (playerGreenCell === randomRedCell) {
       randomRedCell.classList.remove("bg-red-600");
 
       // this is just a precaution
       randomRedCell = cellArray[randomRedIndex + 1];
-      randomGreenCell = cellArray[randomGreenIndex + 2];
+      playerGreenCell = cellArray[randomGreenIndex + 2];
 
       // this adds a new classlist value to our new cells
-      randomGreenCell.classList.add("bg-green-600");
+      playerGreenCell.classList.add("bg-green-600");
       randomRedCell.classList.add("bg-red-600");
-
-      console.log(randomGreenCell, randomRedCell);
     }
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowUp") {
+        console.log("working");
+      }
+    });
   };
+  //This allows us to create the game
   startButton.addEventListener("click", createGrid);
 };
 
-function test() {
-  const game = gameBoard();
-  //   console.log(game.randomGreenCell);
-}
-test();
 gameBoard();
